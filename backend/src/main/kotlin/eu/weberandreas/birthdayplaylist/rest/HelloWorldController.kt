3658a435b89958ba11df7513eb664352cf2c78de
Andreas Weber <info@weberandreas.eu>
2020-06-12 00:29:48 +0200

    Consume rest api with axios

--- backend/src/main/kotlin/eu/weberandreas/birthdayplaylist/rest/HelloWorldController.kt
+++ backend/src/main/kotlin/eu/weberandreas/birthdayplaylist/rest/HelloWorldController.kt
@@ -1,11 +1,13 @@
 package eu.weberandreas.birthdayplaylist.rest
 
 import eu.weberandreas.birthdayplaylist.model.Greeting
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin(origins = ["http://localhost:8081"])
 @RequestMapping("/api")
 class HelloWorldController {
 
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,16 +2,33 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Message from rest api: {{ message }}</h2>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      message: null
+    }
+  },
+  created () {
+    var request = axios.get('http://localhost:8080/api/hello')
+    request
+      .then(response => {
+        this.message = response.data.greeting
+      })
+      .catch(e => {
+        console.log(e)
+      })
   }
 }
 </script>
