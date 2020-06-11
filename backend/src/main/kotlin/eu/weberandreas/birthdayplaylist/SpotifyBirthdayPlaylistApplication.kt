e107ce466bb06dcc09378367e34c73bf561eca3d
Andreas Weber <info@weberandreas.eu>
2020-06-11 17:59:45 +0200

    Init spotify birthday playlist backend

---
+++ backend/src/main/kotlin/eu/weberandreas/birthdayplaylist/SpotifyBirthdayPlaylistApplication.kt
@@ -0,0 +1,11 @@
+package eu.weberandreas.birthdayplaylist
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpotifyBirthdayPlaylistApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpotifyBirthdayPlaylistApplication>(*args)
+}
---
+++ backend/src/test/kotlin/eu/weberandreas/birthdayplaylist/SpotifyBirthdayPlaylistApplicationTests.kt
@@ -0,0 +1,13 @@
+package eu.weberandreas.birthdayplaylist
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpotifyBirthdayPlaylistApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
