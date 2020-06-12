d427b94c09839adb068ce3cafc6216589ed045c1
Andreas Weber <info@weberandreas.eu>
2020-06-12 12:06:05 +0200

    Configure cross origin resources

---
+++ backend/src/main/kotlin/eu/weberandreas/birthdayplaylist/config/Configuration.kt
@@ -0,0 +1,19 @@
+package eu.weberandreas.birthdayplaylist.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class Configuration {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer? {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/api/*").allowedOrigins("http://localhost:8081")
+            }
+        }
+    }
+}
