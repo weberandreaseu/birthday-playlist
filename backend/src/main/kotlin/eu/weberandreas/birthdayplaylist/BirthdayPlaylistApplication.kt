b1970acd84fd96534a1cf15103409b63d62fcc48
Andreas Weber <info@weberandreas.eu>
2020-06-12 17:50:04 +0200

    Change application name

--- backend/src/main/kotlin/eu/weberandreas/birthdayplaylist/BirthdayPlaylistApplication.kt
+++ backend/src/main/kotlin/eu/weberandreas/birthdayplaylist/BirthdayPlaylistApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpotifyBirthdayPlaylistApplication
+class BirthdayPlaylistApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpotifyBirthdayPlaylistApplication>(*args)
+	runApplication<BirthdayPlaylistApplication>(*args)
 }
